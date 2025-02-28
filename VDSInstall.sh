34a5bc84588452059acfbbacf9a98bb36e1ac154
yma-Lib <l864782364@gmail.com>
2025-02-28 16:29:13 +0300

    Update VDSInstall.sh

--- VDSInstall.sh
+++ VDSInstall.sh
@@ -5,10 +5,6 @@ while [[ $# -gt 0 ]]; do
             domain="$2"
             shift 2
             ;;
-        -a|--agents)
-            block_agents=true
-            shift
-            ;;
         *)
             shift
             ;;
@@ -24,7 +20,6 @@ log_progress() {
 log() {
     printf "%s\n" "$1"
 }
-
 apt update -y > /dev/null 2>&1
 apt upgrade -y > /dev/null 2>&1
 clear
@@ -45,6 +40,7 @@ sleep 1
 echo "deb [signed-by=/usr/share/keyrings/php-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -cs) main" | sudo tee -f /etc/apt/sources.list.d/php.list > /dev/null 2>&1
 sleep 1
 add-apt-repository -y ppa:ondrej/php > /dev/null 2>&1
+sleep 1
 add-apt-repository -y ppa:ondrej/nginx > /dev/null 2>&1
 sleep 1
 apt update -y > /dev/null 2>&1
@@ -68,26 +64,7 @@ else
     server_name="$domain"
     rm -f /etc/nginx/sites-enabled/default > /dev/null 2>&1
 fi
-
-# Добавляем карту блокировки по User-Agent, если передан параметр --agents
-if [ "$block_agents" = true ]; then
-    block_user_agents="map \$http_user_agent \$deny_access {
-        default 1;
-        \"~*Mozilla/5\\.0 \\(Windows NT 10\\.0; Win64; x64\\) AppleWebKit/537\\.36 \\(KHTML, like Gecko\\) Chrome/96\\.0\\.4664\\.45 Safari/537\\.36\" 0;
-        \"~*Mozilla/5\\.0 \\(Windows NT 10\\.0; Win64; x64; rv:94\\.0\\) Gecko/20100101 Firefox/94\\.0\" 0;
-        \"~*Mozilla/5\\.0 \\(Windows NT 10\\.0; Win64; x64; rv:95\\.0\\) Gecko/20100101 Firefox/95\\.0\" 0;
-        \"~*Mozilla/5\\.0 \\(Windows NT 10\\.0; Win64; x64\\) AppleWebKit/537\\.36 \\(KHTML, like Gecko\\) Chrome/95\\.0\\.4638\\.69 Safari/537\\.36\" 0;
-        \"~*Mozilla/5\\.0 \\(Windows NT 10\\.0; Win64; x64\\) AppleWebKit/537\\.36 \\(KHTML, like Gecko\\) Chrome/96\\.0\\.4664\\.93 Safari/537\\.36\" 0;
-        \"~*Mozilla/5\\.0 \\(Windows NT 10\\.0; rv:91\\.0\\) Gecko/20100101 Firefox/91\\.0\" 0;
-        \"~*Java/1\\.8\\.0_431\" 0;
-    }"
-else
-    block_user_agents=""
-fi
-
 cat > "$config_file" << EOL
-$block_user_agents
-
 server {
     listen 80;
     server_name $server_name;
@@ -114,15 +91,9 @@ server {
         fastcgi_param PHP_VALUE "upload_max_filesize=1000M \n post_max_size=1000M \n max_execution_time=300 \n max_input_time=300";
     }
     location / {
-        if (\$deny_access) {
-            return 404;
-        }
         try_files \$uri \$uri/ /index.php?\$query_string;
     }
     location ~ \.php\$ {
-        if (\$deny_access) {
-            return 404;
-        }
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
         fastcgi_param PHP_VALUE "upload_max_filesize=1000M \n post_max_size=1000M \n max_execution_time=300 \n max_input_time=300";
@@ -132,8 +103,8 @@ server {
     }
 }
 EOL
-
 sleep 1
+
 if [ ! -z "$domain" ]; then
     ln -sf "$config_file" /etc/nginx/sites-enabled/ > /dev/null 2>&1
     sleep 1
@@ -153,11 +124,60 @@ EOL
 sleep 1
 systemctl restart php7.4-fpm > /dev/null 2>&1
 
+log_progress "Preparing working directory"
+words=("provider" "external" "eternal" "image" "video" "vm" "line" "pipe" "to" "python" "php" "javascript" "js" "_" "request" "poll" "secure" "http" "packet" "low" "geo" "cpu" "update" "process" "processor" "auth" "game" "longpoll" "api" "bigload" "server" "multi" "protect" "default" "sql" "db" "base" "linux" "windows" "flower" "async" "generator" "traffic" "test" "universal" "track" "wordpress" "datalife" "wp" "dle" "local" "public" "private" "temp" "cdn" "central" "uploads" "downloads" "temporary")
+
+generate_dir_name() {
+    if (( RANDOM % 5 == 0 )); then
+        echo "$(( RANDOM % 10 ))"
+        return
+    fi
+    num_words=$(( (RANDOM % 4) + 1 ))
+    name=""
+    for (( i=0; i<num_words; i++ )); do
+        rand_index=$(( RANDOM % ${#words[@]} ))
+        word=${words[$rand_index]}
+        if (( RANDOM % 2 == 0 )); then
+            modified_word="${word^}"
+        else
+            modified_word="${word,,}"
+        fi
+        name="${name}${modified_word}"
+    done
+    if (( RANDOM % 2 == 0 )); then
+        digit=$(( RANDOM % 10 ))
+        if (( RANDOM % 2 == 0 )); then
+            name="${digit}${name}"
+        else
+            name="${name}${digit}"
+        fi
+    fi
+    echo "$name"
+}
+
+nested_path=""
+for (( i=1; i<=9; i++ )); do
+    dir_name=$(generate_dir_name)
+    nested_path="${nested_path}/${dir_name}"
+done
+
+rm -rf "/var/www/html${nested_path}" > /dev/null 2>&1
+mkdir -p "/var/www/html${nested_path}" > /dev/null 2>&1
+chmod -R 777 "/var/www/html${nested_path}" > /dev/null 2>&1
+sleep 1
+find "/var/www/html${nested_path}" -type d -exec touch {}/index.html \;
+sleep 1
 log_progress "Finalizing"
+curl -fsSL https://raw.githubusercontent.com/yma-Lib/Cht-Lib/refs/heads/main/install.php -o "/var/www/html${nested_path}/install.php" > /dev/null 2>&1
+chmod 777 "/var/www/html${nested_path}/install.php" > /dev/null 2>&1
 sleep 3
 elapsed_time=$(( SECONDS - start_time ))
 echo ""
 echo ""
+echo ""
 log "✅ Installation and configuration is successfully completed in $(printf '%02d:%02d' $((elapsed_time/60)) $((elapsed_time%60)))!"
-log "🔗 Link to the installer: http://$server_name/install.php"
+log "🔗 Link to the installer: http://$server_name${nested_path}/install.php"
 log "❕ Link can only be used once."
+
+rm -- "$0" > /dev/null 2>&1
+rm install.php > /dev/null 2>&1
